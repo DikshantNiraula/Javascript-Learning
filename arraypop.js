a4b2c3e036a93765ce3365c25e05df27cdabf0e7
Dikshant Niraula <dikshantniraula351@gmail.com>
2022-06-12 22:17:27 +0545

    Pop in jS Array

---
+++ arraypop.js
@@ -0,0 +1,9 @@
+let arrayData = ["Paul", "John", "Ringo", "David Gimour"];
+
+arrayData.pop("David Gimour"); //No matter what It removes last element //we will use index and slice for removing specific value
+
+let arrayPopDataList = () => {
+    return arrayData;
+};
+
+export default arrayPopDataList;
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 // import arrowFunction from "./arrowFunction.js";
 // import Product from "./constructor.js";
-import arrayDataList from "./arraypush.js";
+// import arrayDataList from "./arraypush.js";
+
+import arrayPopDataList from "./arraypop.js";
+
+//for Array Pop
+console.log(arrayPopDataList());
 
 // For Array Push
-console.log(arrayDataList());
+// console.log(arrayDataList());
 
 //For Product
 // let newProduct = new Product("Blue Pepe Jeans", 5000, "NewPepe");
