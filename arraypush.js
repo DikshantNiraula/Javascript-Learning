708e4c296fb67eb9ad83355bd5180299a52df7ed
Dikshant Niraula <dikshantniraula351@gmail.com>
2022-06-12 22:13:24 +0545

    Push in jS Array

---
+++ arraypush.js
@@ -0,0 +1,12 @@
+let arrayData = ["Paul", "John", "Ringo", "David Gimour"];
+
+arrayData.push("Rogers Waters");
+arrayData.push("David Bowie");
+arrayData.push("Damien Rice");
+arrayData.push("Freddie Mercury");
+
+let arrayDataList = () => {
+    return arrayData;
+};
+
+export default arrayDataList;
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-import arrowFunction from "./arrowFunction.js";
-import Product from "./constructor.js";
+// import arrowFunction from "./arrowFunction.js";
+// import Product from "./constructor.js";
+import arrayDataList from "./arraypush.js";
 
-let newProduct = new Product("Blue Pepe Jeans", 5000, "NewPepe");
-console.log(newProduct.getPrice()); //returns price through a method in Product
+// For Array Push
+console.log(arrayDataList());
 
+//For Product
+// let newProduct = new Product("Blue Pepe Jeans", 5000, "NewPepe");
+// console.log(newProduct.getPrice()); //returns price through a method in Product
+
+// For Arrow Function
 // console.log(arrowFunction(2, 4));
 
 // console.log("Working Fine!!");
